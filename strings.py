dda89245c98957c9fe4c66f59681966e38749ffe
ryanren <wenkun.ren@gmail.com>
2024-03-05 09:48:53 -0600

    strings.py

---
+++ strings.py
@@ -0,0 +1,12 @@
+# name = 'Ryan'
+# age = 32
+
+# print('my name is ' + name + ' and I am ' + str(age))
+
+# print('My name is {name} and I am {age}'.format(name=name, age=age))
+
+# print(f'Hi, my first name is {name} and I am {age} years old!')
+
+s = 'python3 crash'
+
+print(s.capitalize())
