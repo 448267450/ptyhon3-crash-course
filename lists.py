afa251dc29f77cbf261c10fbd5a1ba32456019f6
ryanren <wenkun.ren@gmail.com>
2024-03-05 10:37:20 -0600

    lists.py

---
+++ lists.py
@@ -0,0 +1,25 @@
+numbers = [12,23,443,45,56]
+
+fruits = ['Apples', 'Grapefruit', 'Watermelon']
+
+# numbers2 = list((3,4,5,6,67))
+
+
+# print (len(fruits))
+
+
+# fruits.append('bananas')
+
+# fruits.remove('watermelon')
+
+# fruits.insert(2, 'Strawberries')
+
+# fruits.pop(2)
+
+# fruits.reverse()
+
+# fruits.sort(reverse=True)
+
+fruits[0] = 'blackberries'
+
+print(fruits)
