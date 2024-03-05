1838d08266e40005b4dfcd405fb33cba0d48f401
ryanren <wenkun.ren@gmail.com>
2024-03-05 10:48:30 -0600

    tuples.py

---
+++ tuples.py
@@ -0,0 +1,13 @@
+colors  = ('Black', 'White', 'Yellow')
+# colors2 = tuple(('Black', 'White', 'Yellow'))
+
+# print(colors, colors2)
+# colors2 = ('White')  # str type
+colors2 = ('Black',)  # tuple
+
+
+
+# colors[0] = 'orange' # TypeError: 'tuple' object does not support item assignment
+
+del colors2  # it is deleted
+print(colors2)
