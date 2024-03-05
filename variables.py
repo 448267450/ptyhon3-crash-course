794a5099d6d163b8fc49cecd82c02fa034d9d16d
ryanren <wenkun.ren@gmail.com>
2024-03-05 09:36:02 -0600

    first commit

---
+++ variables.py
@@ -0,0 +1,15 @@
+# x = 1             #int 
+# y = 2.5          #float
+# name = 'John'     # str
+# is_cool = True     #bool 
+
+
+# multiple assignment
+x, y, name, is_cool = (1, 2.5, 'John', True)
+
+a = x + y
+
+x = str(x)
+z = float(y)
+
+print (type(z), z)
