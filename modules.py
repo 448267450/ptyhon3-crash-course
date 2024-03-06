860e023b4402e0cb3b085cde8dc901027922185d
ryanren <wenkun.ren@gmail.com>
2024-03-06 09:16:01 -0600

    class.py

--- modules.py
+++ modules.py
@@ -1,16 +1,25 @@
 import datetime
 from datetime import date
 # import time
-from time import time
-
+# from time import time
 from camelcase import CamelCase
+import validator
+from validator import validate_email
+
 # today = datetime.date.today()
 
-today = date.today()
-# timestamp = time.time()
+# today = date.today()
+# # timestamp = time.time()
 
-# print(timestamp)
+# # print(timestamp)
 
 
-c = CamelCase()
-print(c.hump('hello there world!'))
+# c = CamelCase()
+# print(c.hump('hello there world!'))
+
+email = 'test@test.com'
+
+if validate_email(email):
+    print('Email is valid')
+else:
+    print('Email is bad')
