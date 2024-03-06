88fc63e01c09b0e14c80f7f372013a3964b13d81
ryanren <wenkun.ren@gmail.com>
2024-03-05 23:32:47 -0600

    conditions.py

---
+++ conditions.py
@@ -0,0 +1,33 @@
+x = 10
+y = 10
+
+
+# if x > y:
+#     print(f'{x} is greater than {y}')
+# elif x == y:
+#     print(f'{x} is equal to {y}')
+# else:
+#     print(f'{x} is less than {y}')
+
+
+# if x > 2 and x <= 10:
+#     print(f'{x} is greater than 2 and less than or equal to 10')
+
+# if x > 2 or x <= 20:
+#     print(f'{x} is greater than 2 or less than or equal to 10')
+
+
+
+# if not(x == y):
+#     print(f'{x} is not equal to {y}')
+
+
+numbers = [1,2,3,4,5]
+
+# if x in numbers:
+#     print(x in numbers)
+# else:
+#     print(x not in numbers)
+
+if x is y:
+    print(x is y)
