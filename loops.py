024db461d3d9ac51e48a4a432eaab6e0875c6e60
ryanren <wenkun.ren@gmail.com>
2024-03-05 23:55:01 -0600

    loops.py

---
+++ loops.py
@@ -0,0 +1,19 @@
+people = ['John', 'Paul', 'Sara', 'Susan']
+
+# for p in people:
+#     if p == 'Sara':
+#         continue
+#         # break
+#     print(f'Current Person: {p}')
+
+# for i in range(len(people)):
+#     print(people[i])
+
+
+# for i in range(0, 11):
+#     print(f'Number: {i}')
+
+count = 0
+while count < 10:
+    print(f'Count: {count}')
+    count += 1
